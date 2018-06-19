95b3a6a11e2fd78fddb503bff5f9b45d841857c3
jh2633 <joseph.jr@gmail.com>
2018-06-19 23:22:34 +0100

    add methods for passing tests

--- password.rb
+++ password.rb
@@ -1,1 +1,31 @@
 require 'sinatra'
+class Password
+  MIN_LENGTH = 10
+
+  def initialize(password_string)
+    @password = password_string
+  end
+
+  def strength
+    if min_length(@password)
+      if no_repetition(@password)
+
+      else
+        'weak'
+      end
+    else
+      'weak'
+    end
+  end
+
+  private
+
+
+  def no_repetition(password)
+
+  end
+  def min_length(password)
+    password.length >= MIN_LENGTH
+  end
+
+end
