9c2f1a74b0ab0eee6193123ce4fc097f72ea0b10
jh2633 <joseph.jr@gmail.com>
2018-06-21 00:17:58 +0100

    fix nil assignment, update method names

--- password.rb
+++ password.rb
@@ -4,13 +4,13 @@ class Password
 
   def initialize(password_string, rules = nil, requirement = nil)
     @password = password_string
-    @optional_rules ||= {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
-    @required_rules ||= {min_characters: /.{10,}/}
+    @optional_rules = rules ||= {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
+    @required_rules = requirement ||= {min_characters: /.{10,}/}
   end
 
 
   def strength
-    if meets_required_rules(@password)
+    if meets_required_rules
       @points = 0
       meets_optional_rules
       PWD_SCORE.key?(@points) ? PWD_SCORE[@points] : 'weak'
@@ -20,13 +20,13 @@ class Password
   end
 
   private
-  def meets_required_rules(password)
-    @requirement.all? {|name, pattern| password.match?(pattern)}
+  def meets_required_rules
+    @required_rules.all? {|name, pattern| @password.match?(pattern)}
   end
 
-  def meets_optional_rules(password)
+  def meets_optional_rules
     @optional_rules.each do |name, pattern|
-      if password.match?(pattern)
+      if @password.match?(pattern)
         @points += 1
       end
     end
