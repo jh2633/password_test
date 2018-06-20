907527c1ab7ec58586255cbb817f187f7074d4c6
jh2633 <joseph.jr@gmail.com>
2018-06-20 22:36:36 +0100

    create constant for password scoring, allow strength method to check password rules

--- password.rb
+++ password.rb
@@ -1,54 +1,54 @@
 require 'sinatra'
 class Password
   MIN_LENGTH = 10
+  PWD_SCORE = {3 => 'ok', 4 => 'strong'}
 
-  def initialize(password_string, rules)
+  def initialize(password_string, *rules)
     @password = password_string
-    @rules = rules
+    @rules = rules || {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
   end
 
 
   def strength
-    if min_length(@password)
-      if no_repetition(@password)
-        'strong'
-      else
-        'weak'
+    @points = 0
+    @rules.each do |name, pattern|
+      if @password.match?(pattern)
+        @points += 1
       end
-    else
-      'weak'
     end
+    PWD_SCORE.key?(@points) ? PWD_SCORE[@points] : 'weak'
+
   end
 
   private
-  def uppercase(password)
-    if password.match?(/[A-Z]/)
-      @criteria << :uppercase
-    end
-  end
-
-  def lowercase(password)
-    if password.match?(/[a-z]/)
-      @criteria << :lowercase
-      end
-  end
-
-  def integer(password)
-    if password.match?(/[0-9]/)
-      @criteria << :integer
-    end
-  end
-
-  def special(password)
-    if  password.match?(/\W/)
-      @criteria << :special_character
-    end
-  end
+  # def uppercase(password)
+  #   if password.match?(/[A-Z]/)
+  #     @criteria << :uppercase
+  #   end
+  # end
+  #
+  # def lowercase(password)
+  #   if password.match?(/[a-z]/)
+  #     @criteria << :lowercase
+  #     end
+  # end
+  #
+  # def integer(password)
+  #   if password.match?(/[0-9]/)
+  #     @criteria << :integer
+  #   end
+  # end
+  #
+  # def special(password)
+  #   if  password.match?(/\W/)
+  #     @criteria << :special_character
+  #   end
+  # end
 
   def no_repetition(password)
     password.match?(/'(.)\1{2,}'/)
   end
-  
+
   def min_length(password)
     password.length >= MIN_LENGTH
   end
