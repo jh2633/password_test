a96a272bcd38c1e2689adde27e0e5e53dc161676
jh2633 <joseph.jr@gmail.com>
2018-06-20 23:32:56 +0100

    create additional rule for /required/ rules to pass

--- password.rb
+++ password.rb
@@ -3,54 +3,25 @@ class Password
   MIN_LENGTH = 10
   PWD_SCORE = {3 => 'ok', 4 => 'strong'}
 
-  def initialize(password_string, *rules)
+  def initialize(password_string, rules = nil, requirement = nil)
     @password = password_string
-    @rules = rules || {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
+    @rules ||= {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
+    @requirement ||= {min_characters: /.{10,}/}
   end
 
 
   def strength
-    @points = 0
-    @rules.each do |name, pattern|
-      if @password.match?(pattern)
-        @points += 1
+    if @requirement.all? {|name, pattern| @password.match?(pattern)}
+      @points = 0
+      @rules.each do |name, pattern|
+        if @password.match?(pattern)
+          @points += 1
+        end
       end
+      PWD_SCORE.key?(@points) ? PWD_SCORE[@points] : 'weak'
+    else
+      'weak'
     end
-    PWD_SCORE.key?(@points) ? PWD_SCORE[@points] : 'weak'
-
-  end
-
-  private
-  # def uppercase(password)
-  #   if password.match?(/[A-Z]/)
-  #     @criteria << :uppercase
-  #   end
-  # end
-  #
-  # def lowercase(password)
-  #   if password.match?(/[a-z]/)
-  #     @criteria << :lowercase
-  #     end
-  # end
-  #
-  # def integer(password)
-  #   if password.match?(/[0-9]/)
-  #     @criteria << :integer
-  #   end
-  # end
-  #
-  # def special(password)
-  #   if  password.match?(/\W/)
-  #     @criteria << :special_character
-  #   end
-  # end
-
-  def no_repetition(password)
-    password.match?(/'(.)\1{2,}'/)
-  end
-
-  def min_length(password)
-    password.length >= MIN_LENGTH
   end
 
 end
