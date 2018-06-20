61a75a59d07a90a0bacaecaaf2a4fca8cf9ccbb8
jh2633 <joseph.jr@gmail.com>
2018-06-20 23:38:50 +0100

    extract checking against required and optional rules to private methods

--- password.rb
+++ password.rb
@@ -1,26 +1,34 @@
 require 'sinatra'
 class Password
-  MIN_LENGTH = 10
   PWD_SCORE = {3 => 'ok', 4 => 'strong'}
 
   def initialize(password_string, rules = nil, requirement = nil)
     @password = password_string
-    @rules ||= {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
-    @requirement ||= {min_characters: /.{10,}/}
+    @optional_rules ||= {uppercase: /[A-Z]/, lowercase: /[a-z]/, integer: /[0-9]/, special: /\W/ }
+    @required_rules ||= {min_characters: /.{10,}/}
   end
 
 
   def strength
-    if @requirement.all? {|name, pattern| @password.match?(pattern)}
+    if meets_required_rules(@password)
       @points = 0
-      @rules.each do |name, pattern|
-        if @password.match?(pattern)
-          @points += 1
-        end
-      end
+      meets_optional_rules
       PWD_SCORE.key?(@points) ? PWD_SCORE[@points] : 'weak'
     else
       'weak'
+    end
+  end
+
+  private
+  def meets_required_rules(password)
+    @requirement.all? {|name, pattern| password.match?(pattern)}
+  end
+
+  def meets_optional_rules(password)
+    @optional_rules.each do |name, pattern|
+      if password.match?(pattern)
+        @points += 1
+      end
     end
   end
 
